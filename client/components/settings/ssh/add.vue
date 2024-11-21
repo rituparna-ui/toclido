16f164c1782a1a9911645a9702046ec49bed7e72
Rituparna W <rituparnaw16@gmail.com>
2024-11-21 11:58:22 +0530

    feat(ssh-keys): add key frontend

---
+++ client/components/settings/ssh/add.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <form class="flex flex-col gap-4" @submit.prevent="createSshKey">
+      <Input
+        type="text"
+        class="text-black"
+        placeholder="Name"
+        required
+        name="name"
+      />
+      <Input
+        type="text"
+        class="text-black"
+        placeholder="Public Key"
+        required
+        name="publicKey"
+      />
+      <Button type="submit">Add SSH Key</Button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+const runtimeConfig = useRuntimeConfig();
+const baseUrl = runtimeConfig.public.baseAPI;
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { toast } from "vue-sonner";
+
+const token = useCookie("token");
+
+const createSshKey = async (e) => {
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData.entries());
+  const { name, publicKey } = data;
+  try {
+    const data = await $fetch(baseUrl + "/ssh-keys", {
+      method: "POST",
+      body: {
+        name,
+        publicKey,
+      },
+      headers: {
+        Authorization: `Bearer ${token.value}`,
+      },
+    });
+    toast.success(data.message);
+    e.target.reset();
+  } catch (error) {
+    toast.error(error.data.message);
+  }
+};
+</script>
--- client/pages/settings/index.vue
+++ client/pages/settings/index.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="p-8">
     <h1 class="text-center text-2xl">Settings</h1>
-    <div class="w-full lg:w-1/2 mx-auto"></div>
+    <div class="w-full lg:w-1/2 mx-auto mt-4">
+      <Tabs default-value="account" class="w-full">
+        <TabsList class="w-full bg-[#393E46]">
+          <TabsTrigger class="w-full text-lg" value="account">
+            SSH Keys
+          </TabsTrigger>
+          <TabsTrigger class="w-full text-lg" value="password">
+            CLI Tokens
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          <div>
+            <SettingsSshAdd />
+          </div>
+        </TabsContent>
+        <TabsContent value="password">
+          <div>CLI Tokens</div>
+        </TabsContent>
+      </Tabs>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 definePageMeta({
   middleware: ["auth"],
 });
