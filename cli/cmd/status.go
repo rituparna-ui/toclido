4552ce6db6ba32aeb91ef35b61a60ea2c4acff63
Rituparna W <rituparnaw16@gmail.com>
2024-11-22 02:34:52 +0530

    feat(cli/auth-status): display authentication status

--- cli/cmd/status.go
+++ cli/cmd/status.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -11,7 +13,26 @@ var statusCmd = &cobra.Command{
 	Use:   "status",
 	Short: "Check authentication status",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("status called")
+		dirname, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		if _, err := os.Stat(dirname + "/.toclido-cli"); err == nil {
+			file, err := os.Open(dirname + "/.toclido-cli")
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			defer file.Close()
+			var p Payload
+			json.NewDecoder(file).Decode(&p)
+			user := p.User
+			fmt.Println("\nLogged in as " + user + "\n")
+			return
+		} else {
+			fmt.Println("Not logged in")
+		}
 	},
 }
 
